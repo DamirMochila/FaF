200ad7e5004386492808dd16d79acdc535e90ffb
media314 <aleksandr@3.14media.ru>
2022-02-10 19:53:23 +0300

    Damir molodec reshil zadachku

--- print.py
+++ print.py
@@ -1,2 +1,18 @@
 
-print ('hello damir')
+b = 1
+a = b
+c = b
+d = c
+b = d
+
+damir = 5 + 1
+darina = 6 - damir
+mochila = damir - darina
+mochila_starshiy = damir + darina + mochila
+
+#print ('Результат будет ' + str (mochila_starshiy))
+
+flag = (True and False) or (not True or not False)
+flag_new = (flag and True) and (flag or False) and True
+
+print ('Результат логического выражения - ' + str (flag_new))
