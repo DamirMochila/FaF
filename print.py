51f03263672d2107e9cc2aabb2fee8af48cf3319
DamirMochila <98460318+DamirMochila@users.noreply.github.com>
2022-02-16 23:46:10 +0300

    Update print.py

--- print.py
+++ print.py
@@ -16,3 +16,43 @@ flag = (True and False) or (not True or not False)
 flag_new = (flag and True) and (flag or False) and True
 
 print ('Результат логического выражения - ' + str (flag_new))
+
+d = 2
+a = d + 12
+g = d + a - 6
+
+#print ('Результат евляеться - ' + str (g))
+ 
+a = 7
+d = a
+c = 2 
+w = c + d + a 
+
+#print ('Результат евляеться - ' + str (w))
+
+peremenaia = 23 + 12 
+peremennaia = peremenaia + 21 - 23
+peremennnaia = peremenaia - peremennaia + 3
+
+#print ('Результат евляеться - ' + str (peremennnaia))
+
+f = False 
+t = True
+permena1a = (f and t) or (f and f)
+peremenna1a = (permena1a and t) and (not t or f)
+
+#print ('Результат евляеться - ' + str (peremenna1a))
+
+f = False 
+t = True
+la = (f or t) and t or (t and t)
+lal = t or (t and not f) or (la and f)
+
+#print ('Результат евляеться - ' + str (lal))
+
+f = False 
+t = True  
+k = (f and t) or (f and not t or t) 
+ke = not k and (k or t) and (k and k)
+
+#print ('Результат евляеться - ' + str (ke))
